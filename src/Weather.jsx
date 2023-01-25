378a50cf52ff62f6ffbf54f518bb61329e338ef2
Jonathan <jlbenfield.cccmc@gmail.com>
2023-01-25 18:58:45 -0500

    create Weather component

---
+++ src/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Weather(props) {
+    
+  // api call, using the location being passed into Weather()  
+
+  
+  
+  return (
+    <div>
+        {/* render cards here by passing weather data to card components? */}
+    </div>
+  )
+}
+
+export default Weather
